ccb18495409ac60eb5753a6ac2e0e6d6d88cee9b
Wu Liwei <wuliwei@Wus-MacBook-Pro.local>
2017-01-26 09:33:22 -0800

    combine two functions into one for faster speed

--- primalMethod.jl
+++ primalMethod.jl
@@ -587,96 +587,56 @@ function update_U(U, V, X, r, d1, d2, lambda, rows, vals, stepsize, m)
 	return U, total_obj_new
 end
 
-#X = readdlm("MovieLens1m.csv", ',' , Int64);
-#x = vec(X[:,1]) + 1; # userid starting from 0
-#y = vec(X[:,2]) + 1; # same for movieid
-#v = vec(X[:,3]);
 
-X = readdlm("ml1m_train_ratings.csv", ',' , Int64);
-x = vec(X[:,1]);
-y = vec(X[:,2]);
-v = vec(X[:,3]);
-Y = readdlm("ml1m_test_ratings.csv", ',' , Int64);
-xx = vec(Y[:,1]);
-yy = vec(Y[:,2]);
-vv = vec(Y[:,3]);
 
-#main(x, y, v);
-#main(x, y, v, xx, yy, vv)
-
-function main(x, y, v, xx, yy, vv)
-	# userid; movieid
-	# n = 6040; msize = 3952;
-	# depending on the size of X, read n_users and n_items from python output
-	n = 1496; msize = 3952; 
-	X = sparse(x, y, v, n, msize); # userid by movieid
-	Y = sparse(xx, yy, vv, n, msize);
-	# julia column major 
-	# now moveid by userid
-	X = X'; 
-	Y = Y';
-
-	# too large to debug the algorithm, subset a small set: 500 by 750
-	#X = X[1:500, 1:750];
-	#X = X[1:1000, 1:2000];
-	rows = rowvals(X);
-	vals = nonzeros(X);
-	cols = zeros(Int, size(vals)[1]);
-
-	d2, d1 = size(X);
-	cc = 0;
+function compute_pairwise_error_ndcg(U, V, Y, r, d1, d2, rows_t, vals_t, cols_t, ndcg_k)
+	sum_error = 0.; ndcg_sum = 0.;
 	for i = 1:d1
-		tmp = nzrange(X, i);
-		nowlen = size(tmp)[1];
-		for j = 1:nowlen
-			cc += 1
-			cols[cc] = i
+		tmp = nzrange(Y, i)
+		d2_bar = rows_t[tmp];
+		vals_d2_bar = vals_t[tmp];
+		ui = U[:, i]
+		len = size(d2_bar)[1]
+		score = zeros(len)
+		for j = 1:len
+			J = d2_bar[j];
+			vj = V[:, J]
+			score[j] = dot(ui,vj)
 		end
-	end
-
-	rows_t = rowvals(Y);
-	vals_t = nonzeros(Y);
-	cols_t = zeros(Int, size(vals_t)[1]);
-	cc = 0;
-	for i = 1:d1
-		tmp = nzrange(Y, i);
-		nowlen = size(tmp)[1];
-		for j = 1:nowlen
-			cc += 1
-			cols_t[cc] = i
+		error_this = 0
+		n_comps_this = 0
+		for j in 1:(len - 1)
+			jval = vals_d2_bar[j]
+			for k in (j + 1):len
+				kval = vals_d2_bar[k]
+				if score[j] >= score[k] && jval < kval
+					error_this += 1
+				end
+				if score[j] <= score[k] && jval > kval
+					error_this += 1
+				end
+				n_comps_this += 1
+			end
 		end
+		sum_error += error_this / n_comps_this
+
+
+		p1 = sortperm(score, rev = true)
+		p1 = p1[1:ndcg_k]
+		M1 = vals_d2_bar[p1]
+		p2 = sortperm(vals_d2_bar, rev = true)
+		p2 = p2[1:ndcg_k]
+		M2 = vals_d2_bar[p2]
+		dcg = 0.; dcg_max = 0.
+		for k = 1:ndcg_k
+			dcg += (2 ^ M1[k] - 1) / log2(k + 1)
+			dcg_max += (2 ^ M2[k] - 1) / log2(k + 1)
+		end
+		ndcg_sum += dcg / dcg_max
 	end
-
-
-	r = 100; 
-	lambda = 5000;
-	ndcg_k = 10;
-	# initialize U, V
-	srand(1234)
-	U = 0.1*randn(r, d1);
-	V = 0.1*randn(r, d2);
-	stepsize = 1
-
-	totaltime = 0;
-	for iter in 1:20
-		tic();
-#	println("Outer iteration: ", iter)
-
-@time V, m, nowobj  = update_V(U, V, X, r, d1, d2, lambda, rows, vals, stepsize, cols)
-	
-@time U, nowobj = update_U(U, V, X, r, d1, d2, lambda, rows, vals, stepsize, m)
-
-	 	
-		totaltime += toq();
-
-		# need to add codes for computing pairwise error and NDCG
-	 	pairwise_error = compute_pairwise_error(U, V, Y, r, d1, d2, rows_t, vals_t, cols_t)
-	 	ndcg = computer_NDCG(U, V, Y, r, d1, d2, rows_t, vals_t, cols_t, ndcg_k)
-		println("Iter ", iter, " Time ", totaltime, " obj ", nowobj, " pairwise_error ", pairwise_error, " NDCG ", ndcg)
-
-	end
-#	return V, U
+	return sum_error / d1, ndcg_sum / d1
 end
+
 
 function compute_pairwise_error(U, V, Y, r, d1, d2, rows_t, vals_t, cols_t)
 	sum_error = 0.
@@ -743,4 +703,101 @@ function computer_NDCG(U, V, Y, r, d1, d2, rows_t, vals_t, cols_t, ndcg_k)
 end
 
 
+
+
+#X = readdlm("MovieLens1m.csv", ',' , Int64);
+#x = vec(X[:,1]) + 1; # userid starting from 0
+#y = vec(X[:,2]) + 1; # same for movieid
+#v = vec(X[:,3]);
+
+X = readdlm("ml1m_train_ratings.csv", ',' , Int64);
+x = vec(X[:,1]);
+y = vec(X[:,2]);
+v = vec(X[:,3]);
+Y = readdlm("ml1m_test_ratings.csv", ',' , Int64);
+xx = vec(Y[:,1]);
+yy = vec(Y[:,2]);
+vv = vec(Y[:,3]);
+
+#main(x, y, v);
+#main(x, y, v, xx, yy, vv)
+
+function main(x, y, v, xx, yy, vv)
+	# userid; movieid
+	# n = 6040; msize = 3952;
+	# depending on the size of X, read n_users and n_items from python output
+	n = 1496; msize = 3952; 
+	X = sparse(x, y, v, n, msize); # userid by movieid
+	Y = sparse(xx, yy, vv, n, msize);
+	# julia column major 
+	# now moveid by userid
+	X = X'; 
+	Y = Y';
+
+	# too large to debug the algorithm, subset a small set: 500 by 750
+	#X = X[1:500, 1:750];
+	#X = X[1:1000, 1:2000];
+	rows = rowvals(X);
+	vals = nonzeros(X);
+	cols = zeros(Int, size(vals)[1]);
+
+	d2, d1 = size(X);
+	cc = 0;
+	for i = 1:d1
+		tmp = nzrange(X, i);
+		nowlen = size(tmp)[1];
+		for j = 1:nowlen
+			cc += 1
+			cols[cc] = i
+		end
+	end
+
+	rows_t = rowvals(Y);
+	vals_t = nonzeros(Y);
+	cols_t = zeros(Int, size(vals_t)[1]);
+	cc = 0;
+	for i = 1:d1
+		tmp = nzrange(Y, i);
+		nowlen = size(tmp)[1];
+		for j = 1:nowlen
+			cc += 1
+			cols_t[cc] = i
+		end
+	end
+
+
+	r = 100; 
+	lambda = 5000;
+	ndcg_k = 10;
+	# initialize U, V
+	#srand(1234)
+	U = 0.1*randn(r, d1);
+	V = 0.1*randn(r, d2);
+	stepsize = 1
+
+	totaltime = 0;
+	for iter in 1:10
+		tic();
+#	println("Outer iteration: ", iter)
+
+#@time V, m, nowobj  = update_V(U, V, X, r, d1, d2, lambda, rows, vals, stepsize, cols)
+	
+#@time U, nowobj = update_U(U, V, X, r, d1, d2, lambda, rows, vals, stepsize, m)
+		
+		V, m, nowobj  = update_V(U, V, X, r, d1, d2, lambda, rows, vals, stepsize, cols)
+		U, nowobj = update_U(U, V, X, r, d1, d2, lambda, rows, vals, stepsize, m)
+	 	
+		totaltime += toq();
+
+		# need to add codes for computing pairwise error and NDCG
+
+	 	#pairwise_error = compute_pairwise_error(U, V, Y, r, d1, d2, rows_t, vals_t, cols_t)
+	 	#ndcg = computer_NDCG(U, V, Y, r, d1, d2, rows_t, vals_t, cols_t, ndcg_k)
+
+	 	pairwise_error, ndcg = compute_pairwise_error_ndcg(U, V, Y, r, d1, d2, rows_t, vals_t, cols_t, ndcg_k)
+		println("Iter ", iter, " Time ", totaltime, " obj ", nowobj, " pairwise_error ", pairwise_error, " NDCG ", ndcg)
+
+	end
+#	return V, U
+end
 
