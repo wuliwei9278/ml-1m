89c39b463a5cb58351bccd9664a1a6e74f56abed
wuliwei9278 <wuliwei9278@gmail.com>
2017-01-30 22:20:14 -0800

    changes to printing

--- primalMethod.jl
+++ primalMethod.jl
@@ -775,8 +775,14 @@ function main(x, y, v, xx, yy, vv)
 	V = 0.1*randn(r, d2);
 	stepsize = 1
 
-	totaltime = 0;
-	for iter in 1:10
+	totaltime = 0.00000;
+	println("iter time objective_function pairwise_error NDCG")
+	pairwise_error, ndcg = compute_pairwise_error_ndcg(U, V, Y, r, d1, d2, rows_t, vals_t, cols_t, ndcg_k)
+	m = comp_m(U, V, X, d1, d2, rows, vals, cols)
+	nowobj = objective(m, U, V, X, d1, lambda, rows, vals)
+	println(" ", 0, " ", totaltime, " ", nowobj, " ", pairwise_error, " ", ndcg, ";")
+
+	for iter in 1:20
 		tic();
 #	println("Outer iteration: ", iter)
 
@@ -795,7 +801,7 @@ function main(x, y, v, xx, yy, vv)
 	 	#ndcg = computer_NDCG(U, V, Y, r, d1, d2, rows_t, vals_t, cols_t, ndcg_k)
 
 	 	pairwise_error, ndcg = compute_pairwise_error_ndcg(U, V, Y, r, d1, d2, rows_t, vals_t, cols_t, ndcg_k)
-		println("Iter ", iter, " Time ", totaltime, " obj ", nowobj, " pairwise_error ", pairwise_error, " NDCG ", ndcg)
+		println(" ", iter, " ", totaltime, " ", nowobj, " ", pairwise_error, " ", ndcg, ";")
 
 	end
 #	return V, U
